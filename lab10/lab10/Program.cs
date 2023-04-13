d062176e0f86135e1f5356da42975a132b3f5a9b
Nayjk <98636064+nayjk@users.noreply.github.com>
2023-04-13 12:36:50 +0300

    Add files via upload

---
+++ lab10/lab10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+
+namespace lab10
+{
+    internal class Program
+    {
+        public delegate void WasherCar(Car car);
+        static void Main(string[] args)
+        {
+            Washer wash = new Washer();
+            WasherCar washer;
+            washer = wash.Wash;
+            Car car = new Car("синий","Киа", "20");
+            washer(car);
+        }
+    }
+
+    public class Car
+    {
+        public string color;
+        public string mark;
+        public string number;
+
+        public Car(string color, string mark, string number)
+        {
+            this.color = color;
+            this.mark = mark;
+            this.number = number;
+        }
+    }
+
+    public class Garage
+    {
+        List<string> car = new List<string>();
+    }
+
+    public class Washer
+    {
+        public void Wash(Car car) => Console.WriteLine("Машина марки " + car.mark + " цветом " + car.color + " номером " + car.number + " сейчас на мойке.");
+    }
+    
+}
